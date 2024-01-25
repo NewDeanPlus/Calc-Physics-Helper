501602d7d577fb652020b21f0ca5904445a8136b
Dean <newdeanplus@gmail.com>
2024-01-25 00:34:26 -0500

    Worked on derivative function for some components of term involving constant powers.

--- src/main/java/com/example/NewMath.java
+++ src/main/java/com/example/NewMath.java
@@ -1,16 +1,64 @@
 package com.example;
 
+import com.example.Term.*;
+
 public class NewMath {
 
     public static class DerivationRules {
     
-        public Term PowerRule(Term term){
+        public Term Derivative(Term term){
             //if derivative exists in term, perform power rule
-    
+
+            double coefficient = term.getCoefficient();
+            NumberE e = term.getNumberE();
+            NaturalLog ln = term.getNaturalLog();
+            Cosine cos = term.getCosine();
+            Sine sin = term.getSine();
+            Tangent tan = term.getTangent();
+            Variable[] variables = term.getVariables();
+
+        //derivative of e
+            if(e.power.isConstant == true){
+                coefficient *= Math.pow(Math.E, e.power.getCoefficient());
+            }else{
+                //TODO - idk, sounds complicated
+            }
+
+        //derivative of ln
+
+        //trig derivatives
+
+        //variable derivatives (power rule)
+                Variable v;
+            for(int i=0; i<variables.length; i++){
+                    v = variables[i];
+                if(v.power.isConstant){
+                    coefficient *= v.power.getCoefficient();
+                    v.power.setCoefficient(v.power.getCoefficient() - 1);
+                }else{
+                    //TODO - idk, i'll work on it
+                }
+            }
+
+            return term;}
+
+        }
+
+        public Term ChainRule(Term term){
+            return term
+        }
+
+        public Term PartialDerivative(Term term, Variable withRespectTo){
             return term;
         }
 
-        public 
+        public Term IndefiniteIntegral(Term term, Variable withRespectTo, boolean includeC){
+            return term;
+        }
+
+        public Term DefiniteIntegral(Term term, Variable withRespectTo, Term upperLimit, Term lowerLimit){
+            return term;
+        }
 
     }
     
--- src/main/java/com/example/Term.java
+++ src/main/java/com/example/Term.java
@@ -9,9 +9,11 @@ public class Term {
     private Cosine cos;
     private Sine sin;
     private Tangent tan;
-    private Integral integ;
-    private Derivative deriv;
+    // private Integral integ;
+    //private Derivative deriv;
     private Variable[] variables;
+
+    public boolean isConstant = false;
 
 
     public Term(
@@ -24,8 +26,8 @@ public class Term {
         Cosine cos,
         Sine sin,
         Tangent tan,
-        Integral integ,
-        Derivative deriv,
+        // Integral integ,
+        // Derivative deriv,
         
         Variable... variables){
 
@@ -36,10 +38,20 @@ public class Term {
             this.cos = cos;
             this.sin = sin;
             this.tan = tan;
-            this.integ = integ;
-            this.deriv = deriv;
+            // this.integ = integ;
+            // this.deriv = deriv;
             this.variables = variables;
 
+    }
+
+    public Term(double coefficient){
+        this.coefficient = coefficient;
+        this.isConstant = true;
+    }
+
+    public Term(double coefficient, Variable... variables){
+        this.coefficient = coefficient;
+        this.variables = variables;
     }
 
     public Term(){}
@@ -56,125 +68,125 @@ public class Term {
 
     public class NumberE{
             boolean ifExists;
-            double power;
-        public NumberE(double p){
+            public Term power;
+        public NumberE(Term p){
             this.power = p;
             this.ifExists = true;
         }
 
-        public NumberE(){  //no e in equation, e^0 equals 1
+        public NumberE(String na){  //no e in equation, e^0 equals 1
             this.ifExists = false;
         }
     }
 
     public class NaturalLog{
             boolean ifExists;
-            double power;
+            Term power;
             Term containedTerm;
-        public NaturalLog(double p, Term t){
+        public NaturalLog(Term p, Term t){
             this.power = p;
             this.containedTerm = t;
             this.ifExists = true;
         }
 
-        public NaturalLog(){
+        public NaturalLog(String na){
             this.ifExists = false;
         }
     }
 
     public class Cosine{
             boolean ifExists;
-            double power;
+            Term power;
             Term containedTerm;
-        public Cosine(double p, Term t){
+        public Cosine(Term p, Term t){
             this.power = p;
             this.containedTerm = t;
             this.ifExists = true;
         }
 
-        public Cosine(){
+        public Cosine(String na){
             this.ifExists = false;
         }
     }
 
     public class Sine{
             boolean ifExists;
-            double power;
+            Term power;
             Term containedTerm;
-        public Sine(double p, Term t){
+        public Sine(Term p, Term t){
             this.power = p;
             this.containedTerm = t;
             this.ifExists = true;
         }
 
-        public Sine(){
+        public Sine(String na){
             this.ifExists = false;
         }
     }
 
     public class Tangent{
             boolean ifExists;
-            double power;
+            Term power;
             Term containedTerm;
-        public Tangent(double p, Term t){
+        public Tangent(Term p, Term t){
             this.power = p;
             this.containedTerm = t;
             this.ifExists = true;
         }
 
-        public Tangent(){
+        public Tangent(String na){
             this.ifExists = false;
         }
     }
 
-    public class Integral{
-            boolean ifExists;
-            char axisOfIntegration;
-            double upperLimit;
-            double lowerLimit;
-        public Integral(char c){  //indefinite integral
-            this.axisOfIntegration = c;
-            this.ifExists = true;
-        }
+    // public class Integral{
+    //         boolean ifExists;
+    //         char axisOfIntegration;
+    //         double upperLimit;
+    //         double lowerLimit;
+    //     public Integral(char c){  //indefinite integral
+    //         this.axisOfIntegration = c;
+    //         this.ifExists = true;
+    //     }
 
-        public Integral(char c, double upper, double lower){  //definite integral
-            this.axisOfIntegration = c;
-            this.upperLimit = upper;
-            this.lowerLimit = lower;
-            this.ifExists = false;
-        }
+    //     public Integral(char c, double upper, double lower){  //definite integral
+    //         this.axisOfIntegration = c;
+    //         this.upperLimit = upper;
+    //         this.lowerLimit = lower;
+    //         this.ifExists = false;
+    //     }
 
-        public Integral(){
-            this.ifExists = false;
-        }
-    }
+    //     public Integral(String na){
+    //         this.ifExists = false;
+    //     }
+    // }
 
-    public class Derivative{  //appears as (d/dx) and so on
-            boolean ifExists;
-            char axisOfDerivation;
-            boolean ifPartial;
-        public Derivative(char c, boolean b){
-            this.axisOfDerivation = c;
-            this.ifPartial = b;
-            this.ifExists = true;
-        }
+    // public class Derivative{  //appears as (d/dx) and so on
+    //         boolean ifExists;
+    //         char axisOfDerivation;
+    //         boolean ifPartial;
+    //     public Derivative(char c, boolean b){
+    //         this.axisOfDerivation = c;
+    //         this.ifPartial = b;
+    //         this.ifExists = true;
+    //     }
 
-        public Derivative(){
-            this.ifExists = false;
-        }
-    }
+    //     public Derivative(){
+    //         this.ifExists = false;
+    //     }
+    // }
 
     public class Variable{
             boolean ifExists;
             char variableName;
-            double power;
-        public Variable(char c, double p){
+            Term power;
+        public Variable(char c, Term p){
             this.variableName = c;
             this.power = p;
             this.ifExists = true;
         }
 
-        public Variable(){
+        public Variable(String na){
             this.ifExists = false;
         }
     }
@@ -208,20 +220,52 @@ public class Term {
         return this.tan;
     }
 
-    public Integral getIntegral(){
-        return this.integ;
-    }
+    // public Integral getIntegral(){
+    //     return this.integ;
+    // }
 
-    public Derivative getDerivative(){
-        return this.deriv;
-    }
+    // public Derivative getDerivative(){
+    //     return this.deriv;
+    // }
 
     public Variable getVariable(int index){
         if(index > variables.length){
-            return new Variable();
+            return new Variable("na");
         }else{
             return variables[index];
         }
     }
+
+    public Variable[] getVariables(){
+        return variables;
+    }
+
+    //setters
+
+    public void setCoefficient(double a){
+        this.coefficient = a;
+    }
+
+    public void setNumberE(NumberE a){
+        this.e = a;
+    }
+
+    public void setNaturalLog(NaturalLog a){
+        this.ln = a;
+    }
+
+    public void setCosine(Cosine a){
+        this.cos = a;
+    }
+
+    public void setSine(Sine a){
+        this.sin = a;
+    }
+
+    public void setTangent(Tangent a){
+        this.tan = a;
+    }
+
+    
 
 }
