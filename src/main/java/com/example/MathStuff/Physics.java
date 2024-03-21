3e5c4af63be6cac124971453a8bda976f183e828
Dean <newdeanplus@gmail.com>
2024-03-21 12:12:20 -0400

    Added physics class for processing physics and engineering problems later

---
+++ src/main/java/com/example/MathStuff/Physics.java
@@ -0,0 +1,24 @@
+package com.example.MathStuff;
+
+public class Physics {
+
+    public class Kinematics{
+            //kinematics
+            int vx; //x velocity
+            int vy; //y velocity
+            int vx0; //init x velocity
+            int vy0; //init y velocity 
+            int ax; //x acceleration
+            int ay; //y acceleration
+            int t; //time
+            int t0; //init time
+            int x; //x pos
+            int y; //y pos
+            int x0; //init x pos
+            int y0; //init y pos
+
+            Kinematics(String x0, String x, String vx0, String vx, String ax, String t0, String t){
+                //initialize variables so that some can be missing
+            }
+    }
+}
