be2b2bf7f0303f357716148e6e9cc6e8ac0b1823
Dean <newdeanplus@gmail.com>
2024-03-21 11:52:34 -0400

    Working on equation formatting

--- src/main/java/com/example/MathStuff/Equation.java
+++ src/main/java/com/example/MathStuff/Equation.java
@@ -9,6 +9,13 @@ public class Equation {
         this.terms = terms;
         //create some kind of system to link terms using operators (+,-,*,/) while still allowing for freedom of parentheses
         //also create functions that allow program to convert between objects and plain text, but won't need that for a while
+
+        //save equations as strings with terms contained inside, for example
+        // ((x + 1) * ((y^2)/(6x^3))) = 2 converted into
+        // ((A) * ((B)/(C))) - D where
+        // A = x + 1, B = y^2, C = 6x^3, D = -2
+
+        //TODO - implement this in code
     }
 
     public Term getTerm(int index){
