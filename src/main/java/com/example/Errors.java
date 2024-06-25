278dfaedc45cb5f002e3da391dc59f0086ef307a
Dean <newdeanplus@gmail.com>
2024-06-25 18:32:55 -0400

    Added some new errors for later

--- src/main/java/com/example/Errors.java
+++ src/main/java/com/example/Errors.java
@@ -1,8 +1,21 @@
 package com.example;
 
 public final class Errors {
+
+    public enum errors {
+        rareExpression, trigError
+    }
+    public static String msg;
     
-    public static void printError(String msg){
+    public static void printError(errors errorType){
+        switch(errorType){
+            case rareExpression: msg = "The program was not prepared for this kind of expression. Please report as feedback.";
+                break;
+            case trigError: msg = "That's some weird trig shit. Please report as feedback.";
+                break;
+            default: msg = "Unknown Error";
+        }
+
         System.out.println("Error: "+msg);
     }
 
